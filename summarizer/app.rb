97906988bd2fba211b99b230ef6e7f1699bef788
osadake212 <osadake.212@gmail.com>
2016-09-21 10:49:13 +0900

    show slow query summary - add configuration - summarize in app.rb

--- summarizer/app.rb
+++ summarizer/app.rb
@@ -2,12 +2,14 @@ require 'sinatra'
 require 'yaml'
 require './query_summarizer'
 require './access_summarizer'
+require './slow_query_summarizer'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 get '/' do
   @query_summary = QuerySummarizer.new(config['query_log']).summarize
   @access_summary = AccessSummarizer.new(config['access_log']).summarize
+  @slow_query_summary = SlowQuerySummarizer.new(config['slow_query_log']).summarize
   erb :index
 end
 
